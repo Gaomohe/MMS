799a03db1899ad7f08f6616c68f51002570a38ba
jackzhu <111@qq.com>
2024-07-08 11:53:41 +0800

    测试

---
+++ src/com/servlet/ApprovalServlet.java
@@ -0,0 +1,24 @@
+package com.servlet;
+
+import com.util.BaseServlet;
+import com.util.init.StringDeal;
+import com.util.init.ToJSON;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/approval")
+public class ApprovalServlet extends BaseServlet {
+    @Override
+    public Class<?> getServlet() {
+        return ApprovalServlet.class;
+    }
+    public String getMenuBtn(HttpServletRequest request, HttpServletResponse response){
+        return "/medicine/approveSysrem/purchaseApproval/purchaseList";
+    }
+    public void getAll(HttpServletRequest request, HttpServletResponse response){
+        ToJSON.toJson(response,null);
+    }
+
+}
