6d6bc42c8c31f2e550c3499c14f826188d23cf66
JR <1747659891@qq.com>
2024-07-30 23:53:00 +0800

    修改有效日期bug

--- web/medicinejs/qualityManage/qualityTest/qualityInfo.js
+++ web/medicinejs/qualityManage/qualityTest/qualityInfo.js
@@ -11,6 +11,9 @@ layui.use(['form', 'layedit', 'laydate','jquery','cascader'], function(){
     $("#tijiao").click(function(){
         var id = $("#id").val();
         var usefulLife = $("#usefulLife").val();
+        if(!isValidDateTime(usefulLife)){
+            usefulLife +="00:00:00";
+        }
         $.ajax({
             url:"/quality?action=upUsefulLife",
             data:{
@@ -42,6 +45,17 @@ layui.use(['form', 'layedit', 'laydate','jquery','cascader'], function(){
         type: 'datetime',
 
     });
+
+    //正则表达式判断有效期格式
+    function isValidDateTime(dateTime) {
+        // 定义正则表达式
+        // 这里的正则表达式假设年份为4位数字，月份和日期为1或2位数字，小时、分钟和秒为2位数字
+        // 并且考虑到了月份和日期可能只有一位数字时前面会有0的情况
+        var regex = /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/;
+
+        // 使用正则表达式测试字符串
+        return regex.test(dateTime);
+    }
 
 });
 
