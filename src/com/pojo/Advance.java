3df90771b8433fe7ff032d4b3113de504a4dcf51
GZX <gaozongxian@ahyzyx.cn>
2025-03-28 15:51:37 +0800

    添加lombok依赖包

--- src/com/pojo/Advance.java
+++ src/com/pojo/Advance.java
@@ -1,7 +1,9 @@
 package com.pojo;
 
+import lombok.Data;
 
 //预付款数据
+@Data
 public class Advance {
     private int orderId;  //批次编号
     private String mname; //商品名称
@@ -19,132 +21,4 @@ public class Advance {
     private String state;   //单据状态
     private String times;   //单据时间
     private String account; //账户
-
-    public int getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(int orderId) {
-        this.orderId = orderId;
-    }
-
-    public String getMname() {
-        return mname;
-    }
-
-    public void setMname(String mname) {
-        this.mname = mname;
-    }
-
-    public String getKind() {
-        return kind;
-    }
-
-    public void setKind(String kind) {
-        this.kind = kind;
-    }
-
-    public String getSpecification() {
-        return specification;
-    }
-
-    public void setSpecification(String specification) {
-        this.specification = specification;
-    }
-
-    public String getDrugFrom() {
-        return drugFrom;
-    }
-
-    public void setDrugFrom(String drugFrom) {
-        this.drugFrom = drugFrom;
-    }
-
-    public String getSupplier() {
-        return supplier;
-    }
-
-    public void setSupplier(String supplier) {
-        this.supplier = supplier;
-    }
-
-    public String getUnit() {
-        return unit;
-    }
-
-    public void setUnit(String unit) {
-        this.unit = unit;
-    }
-
-    public String getProductDate() {
-        return productDate;
-    }
-
-    public void setProductDate(String productDate) {
-        this.productDate = productDate;
-    }
-
-    public double getBuyPrice() {
-        return buyPrice;
-    }
-
-    public void setBuyPrice(double buyPrice) {
-        this.buyPrice = buyPrice;
-    }
-
-    public int getNumber() {
-        return number;
-    }
-
-    public void setNumber(int number) {
-        this.number = number;
-    }
-
-    public String getPercent() {
-        return percent;
-    }
-
-    public void setPercent(String percent) {
-        this.percent = percent;
-    }
-
-    public String getOpera() {
-        return opera;
-    }
-
-    public void setOpera(String opera) {
-        this.opera = opera;
-    }
-
-    public String getFinence() {
-        return finence;
-    }
-
-    public void setFinence(String finence) {
-        this.finence = finence;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getTimes() {
-        return times;
-    }
-
-    public void setTimes(String times) {
-        this.times = times;
-    }
-
-    public String getAccount() {
-        return account;
-    }
-
-    public void setAccount(String account) {
-        this.account = account;
-    }
 }
