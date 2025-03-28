f5d7ffc3aacaec9f36a003b9f6a4f166c74abc5b
GZX <gaozongxian@ahyzyx.cn>
2025-03-28 17:48:05 +0800

    MMS测试提交

--- src/com/pojo/MedicineOrder.java
+++ src/com/pojo/MedicineOrder.java
@@ -1,22 +1,9 @@
 package com.pojo;
 
+import lombok.Data;
+
+@Data
 public class MedicineOrder {
     private int mId;
     private int orderId;
-
-    public int getmId() {
-        return mId;
-    }
-
-    public void setmId(int mId) {
-        this.mId = mId;
-    }
-
-    public int getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(int orderId) {
-        this.orderId = orderId;
-    }
 }
